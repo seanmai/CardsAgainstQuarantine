0c41a931ee4a6f5ba0d0f9938578ddaf29ad2bb7
Jacky <jackyjwu86@gmail.com>
2020-12-04 16:42:30 -0700

    Setup login page UI and added routes to UI containers

--- cards-against-quarantine/src/App.js
+++ cards-against-quarantine/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import CreateCard from './components/create-card.component';
 import CardsList from './components/cards-list.component';
 
-
+import Login from './container/Login/Login';
+import Lobby from './container/Lobby/Lobby';
+import Room from './container/Room/Room';
 
 function App() {
   return (
@@ -12,6 +14,12 @@ function App() {
       {/* <Route path="/" exact component={}/> */}
       <Route path="/cards" exact component={CardsList}/>
       <Route path="/cards/create" exact component={CreateCard}/>
+
+      {/* Routes to check UI containers */}
+      <Route path='/login' exact component={Login}/>
+      <Route path='/lobby' exact component={Lobby}/>
+      <Route path='/room' exact component={Room}/>
+      
     </Router>
   );
 }
---
+++ cards-against-quarantine/src/container/Room/Room.js
@@ -0,0 +1,16 @@
+// Design the game room 
+
+import React, { Component } from 'react';
+import './Room.css'
+
+import Card from '../../components/Card/Card';
+
+export default class Room extends Component {
+    render() {
+
+        return (
+            <Card text={"Card Description"}></Card>
+        );
+    }
+}
+
