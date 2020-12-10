f2c0503b28d9a356e7d09bb17a018af4415cd9ce
Andy Tran <andytran636@gmail.com>
2020-12-09 21:36:13 -0700

    style chat and fix some performance issues

--- cards-against-quarantine/src/components/Chat/Chat.js
+++ cards-against-quarantine/src/components/Chat/Chat.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import socketIO from 'socket.io-client';
-
+import './Chat.css';
 const socket = socketIO('http://localhost:4000');
 
 const Chat = () => {
     
     const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
 
     const handleMessageChange = (e) => {setMessage(e.target.value)};
 
@@ -20,10 +19,6 @@ const Chat = () => {
         setMessage('');
     };
 
-    // const receiveMessage = () => {
-        
-    // }
-
     // NOTE TO ANDY:
     // In routes/game.js, change
     // socket.to(data.gameId).emit('message-broadcast', data);
@@ -32,33 +27,28 @@ const Chat = () => {
     // if you want to test. we currently do not have it joined per room so.
     useEffect(() => {
         socket.on('message-broadcast', data => {
-            setMessages([...messages, data.message]);
+            let newMessage = document.createElement("P");
+            newMessage.innerText = data.message;
+            document.querySelector("#messages").appendChild(newMessage);
         });
-    }, [messages]);
-
-    console.log("message");
-    console.log(message);
-    console.log("messages");
-    console.log(messages);
+    }, []);
 
     return (
         <div id="chat-container">
             <div id="messages-container">
-                <div className="message">
-                    {messages.map((message) => {
-                        return <p key={message}>{message}</p>
-                    })}
+                <div id="messages">
+
                 </div>
             </div>
             <form id="chat-box" onSubmit={handleSubmit}>
                 <input 
                     value={message} 
                     placeholder="Type a message..." 
-                    id="m" 
+                    id="type-input" 
                     autoComplete="off" 
                     onChange={handleMessageChange}
                 />
-                <input type="submit" value="Send"/>
+                <input id="send-button" type="submit" value="Send"/>
             </form>
         </div>
     );
