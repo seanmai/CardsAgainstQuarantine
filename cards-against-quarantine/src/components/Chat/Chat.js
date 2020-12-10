55041917c82e6f67ea7d2148bced3a1513325695
Sean Mai <seandmai@gmail.com>
2020-12-09 20:17:21 -0700

    Added send/recieve message client side

--- cards-against-quarantine/src/components/Chat/Chat.js
+++ cards-against-quarantine/src/components/Chat/Chat.js
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import socketIO from 'socket.io-client';
 
-// const socket = socketIO('https://localhost:4000');
-const messages = ['hello', 'test', 'test123'];
+const socket = socketIO('http://localhost:4000');
 
 const Chat = () => {
-
+    
     const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
 
     const handleMessageChange = (e) => {setMessage(e.target.value)};
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const data = {};
+        data.gameId = '';
+        data.username = '';
+        data.message = message;
+        socket.emit("message", data);
         setMessage('');
     };
 
-    const receiveMessage = () => {
+    // const receiveMessage = () => {
         
-    }
+    // }
+
+    // NOTE TO ANDY:
+    // In routes/game.js, change
+    // socket.to(data.gameId).emit('message-broadcast', data);
+    // to
+    // socket.emit('message-broadcast', data);
+    // if you want to test. we currently do not have it joined per room so.
+    useEffect(() => {
+        socket.on('message-broadcast', data => {
+            setMessages([...messages, data.message]);
+        });
+    }, [messages]);
+
+    console.log("message");
+    console.log(message);
+    console.log("messages");
+    console.log(messages);
 
     return (
         <div id="chat-container">
